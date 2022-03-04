bf5f90bd0eec3727d7d82a595e556edfbaadf125
Arik Alexandrov <arikxl@gmail.com>
2022-03-04 21:00:08 +0200

    animation

--- js.js
+++ js.js
@@ -10,6 +10,7 @@ $('document').ready(function(){
         $('.img1').css('width', '50%');
     });
 
+
     $('.img1').hover(removeColor, setColor);
     function removeColor(){
         $('.img1').css('filter', 'grayscale(100%)');
@@ -30,4 +31,34 @@ $('document').ready(function(){
         $('li:odd').fadeToggle();
     };
     
+
+    $('.img2').css('height', '250px');
+    $('.flex > button').css('background-color', 'orange','flex', '1');
+    
+    $('.btn-hide').click(function(){
+        $('.img2').hide(2000);        
+    })
+    $('.btn-show').click(function(){
+        $('.img2').show(2000);        
+    })
+    $('.btn-toggle').click(function(){
+        $('.img2').toggle(3000);        
+    })
+    $('.btn-slide').click(function(){
+        $('.img2').slideToggle(3000);        
+    })
+    $('.btn-stop').click(function(){
+        $('.img2').stop();        
+    })
+
+
+    $('.btn7').click(function(){
+        $('.img3').animate({
+            left:'1550px',
+            opacity:0.5,
+            height: '300px',
+            width: '400px'
+        },'fast')
+    })
+
 });
