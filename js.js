ddbaf13d002ae1b4bd6ea008b6535e9a3f147932
Arik Alexandrov <arikxl@gmail.com>
2022-03-04 20:22:16 +0200

    events

--- js.js
+++ js.js
@@ -1,17 +1,33 @@
 'use strict'
 
-$('.btn1').click(fade);
-$('.btn2').click(changeStyle);
+$('document').ready(function(){
 
-function fade() {
-    $('.img1').fadeToggle(3000);
-};
+    $('.btn1').click(function(){
+        $('.img1').fadeToggle(3000);
+    });
 
-function changeStyle() {
-    $('section').css('background-color', 'lightblue');
-    $('#p-container2').css('color', 'brown');
-    $('.p1').css('font-style', 'italic');
-    $('.p3, .p2').css('text-transform', 'uppercase');
-    $('li:first-child, h1').css('text-transform', 'capitalize');
-    $('li:odd').fadeToggle();
-};
+    $('.img1').dblclick(function(){
+        $('.img1').css('width', '50%');
+    });
+
+    $('.img1').hover(removeColor, setColor);
+    function removeColor(){
+        $('.img1').css('filter', 'grayscale(100%)');
+    }
+    function setColor(){
+        $('.img1').css('filter', 'grayscale(0)');
+    }
+    
+    
+    $('.btn2').click(changeStyle);
+    
+    function changeStyle() {
+        $('section').css('background-color', 'lightblue');
+        $('#p-container2').css('color', 'brown');
+        $('.p1').css('font-style', 'italic');
+        $('.p3, .p2').css('text-transform', 'uppercase');
+        $('li:first-child, h1').css('text-transform', 'capitalize');
+        $('li:odd').fadeToggle();
+    };
+    
+});
